0688adfd4742577f9b9b14beda8a5ed0941671a1
Vladyslav Filonenko <oladik05@gmail.com>
2024-03-22 00:13:26 +0200

    make build

---
+++ index.0f3c3917.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=(e.clientHeight-t.clientHeight)/2,i=(e.clientWidth-t.clientWidth)/2;t.style.top=l+"px",t.style.left=i+"px";
+//# sourceMappingURL=index.0f3c3917.js.map
